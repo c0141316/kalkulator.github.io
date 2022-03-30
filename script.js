f296d46aa4fd93103c51c7e9ccac0dfb07c4b4f0
Julio CM <79083058+c0141316@users.noreply.github.com>
2022-03-30 21:47:08 +0800

    Update Javascript

---
+++ script.js
@@ -0,0 +1,106 @@
+let prevNumber = '';
+let calculationOperator = '';
+let currentNumber = '0';
+let calcbin = false;
+
+const inputNumber = (number) => {
+  if(currentNumber === '0') {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  }
+}
+
+const inputOperator = (operator) => {
+  if (calculationOperator === '') {
+    prevNumber = currentNumber;
+  }
+  calculationOperator = operator;
+  currentNumber = '';
+}
+
+const calculate = () => {
+  let result = '';
+  switch(calculationOperator) {
+    case "+":
+      result = parseFloat(prevNumber) + parseFloat(currentNumber);
+      break
+    case "-":
+      result = parseFloat(prevNumber) - parseFloat(currentNumber);
+      break
+    case "*":
+      result = parseFloat(prevNumber) * parseFloat(currentNumber);
+      break
+    case "/":
+      result = parseFloat(prevNumber) / parseFloat(currentNumber);
+      break
+    case "%":
+      result = (parseFloat(prevNumber) / 100) * parseFloat(currentNumber);
+      break
+    default:
+      break
+  }
+  currentNumber = result;
+  calculationOperator = '';
+}
+
+const clearAll = () => {
+  prevNumber = '';
+  calculationOperator = '';
+  currentNumber = '0';
+}
+
+inputDecimal = (dot) => {
+  if(currentNumber.includes('.')) {
+    return
+  }
+
+  currentNumber += dot;
+}
+
+const calculatorScreen = document.querySelector(".calculator-screen");
+
+const updateScreen = (number) => {
+  calculatorScreen.value = number;
+}
+
+const numbers = document.querySelectorAll(".number");
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    inputNumber(event.target.value);
+    updateScreen(currentNumber);
+  })
+})
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value);
+  })
+})
+
+
+const equalSign = document.querySelector(".equal-sign");
+
+equalSign.addEventListener("click", () => {
+  calculate();
+  updateScreen(currentNumber);
+})
+
+
+const clearBtn = document.querySelector(".all-clear");
+
+clearBtn.addEventListener("click", () => {
+  clearAll();
+  updateScreen(currentNumber);
+})
+
+
+const decimal = document.querySelector(".decimal");
+
+decimal.addEventListener("click", (event) => {
+  inputDecimal(event.target.value);
+  updateScreen(currentNumber);
+})
